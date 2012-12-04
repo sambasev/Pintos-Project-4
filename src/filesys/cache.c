41a66fdf32fa63d20ffd1e7f62327ddee8e030e8
Sambasevam Shanmugam <sambasevam@gmail.com>
2012-12-04 02:34:25 -0800

    Added hash table support

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -7,6 +7,9 @@ struct cache_entry
    struct hash_elem hash_elem; /* Each cache_entry is an element in the buffer_cache hash table */
 }
 
+void buffer_cache_init (void)
+{
+}
 /* Best effort cache_read */
 int block_cache_read (struct block *block, block_sector_t sector, void *buffer)
 {
@@ -23,7 +26,7 @@ void block_cache_write (struct block *block, block_sector_t sector, const void *
 {
    if (!cache_write (buffer, sector))
      {
-        evict_cache_line ();
+        evict_cache_line ();   /* Cache is full, evict cache line  */
         cache_insert (sector, WRITE);
         set_dirty (sector);
      }
@@ -63,5 +66,39 @@ void evict_cache_line(void)
 {
    /* Least Recently Used Algorithm */
 }
+
+
+
+/* HASH TABLE ACCESSES FOR BUFFER CACHE */
                                       
+unsigned block_hash (const struct hash_elem *p_, void *aux UNUSED)
+{
+  const struct cache_entry *p = hash_entry (p_, struct cache_entry, hash_elem);
+  return hash_bytes (&p->block, sizeof p->block);
+}
+
+bool block_less (const struct hash_elem *a_, const struct hash_elem *b_,
+                void *aux UNUSED)
+{
+  const struct cache_entry *a = hash_entry (a_, struct cache_entry, hash_elem);
+  const struct cache_entry *b = hash_entry (b_, struct cache_entry, hash_elem);
+
+  return a->block < b->block;
+}
+
+struct cache_entry * block_lookup (void *address)
+{
+  struct thread *t = thread_current();
+  struct cache_entry p;
+  struct hash_elem *e;
+  p.block = address;
+  e = hash_find (&t->buffer_cache, &p.hash_elem);
+  return e != NULL ? hash_entry (e, struct cache_entry, hash_elem) : NULL;
+}
+
+void insert_cache (struct cache_entry * entry)
+{
+  struct thread *t = thread_current();
+  hash_insert (&t->buffer_cache, &cache_entry->hash_elem);
+}
 
--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -2,12 +2,14 @@
 
 struct cache_entry
 {
-   void * block;               /* Pointer to a block */
+   void * block;               /* Pointer to a data block - used as key for hash table */
+   void * data;		       /* Pointer to the actual data */	
    bool dirty;                 /* Indicates if this entry was modified */
    bool read_only;             /* R/W */
    struct hash_elem hash_elem; /* Each cache_entry is an element in the buffer_cache hash table */
 }
 
+void buffer_cache_init (void);
 int block_cache_read (struct block *block, block_sector_t sector, void *buffer);
 void block_cache_write (struct block *block, block_sector_t sector, const void *buffer);
 void cache_insert (block_sector_t sector);
